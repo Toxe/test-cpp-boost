e3eb9e52531df2c99874d624bb8e82a622258906
Toxe <toxe@toxe.net>
2019-06-12 11:45:16 +0200

    stream_to_log_file.cpp

---
+++ stream_to_log_file.cpp
@@ -0,0 +1,13 @@
+#include <ostream>
+#include <boost/iostreams/device/file.hpp>
+#include <boost/iostreams/stream.hpp>
+
+namespace io = boost::iostreams;
+
+int main()
+{
+    io::stream_buffer<io::file_sink> buf("log.txt");
+    std::ostream out(&buf);
+
+    out << "Write to log file." << std::endl;
+}
